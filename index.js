6547b3aa1c9e1581563cf8927b078b66604a79b5
DimaCrafter <backup.dimacrafter@gmail.com>
2022-03-04 16:43:48 +0400

    Experemental package type universalization

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-// This file for regular usage
-const API = require('./api');
+// This file for regular usage, non-browser environments not supported
+if (typeof window !== 'undefined') {
+	const API = require('./api');
 
-let isDev = process.env.NODE_ENV === 'development';
-API.settings.dev = (enabled = null) => {
-	if (enabled === null) return isDev;
-	isDev = enabled;
-	if (enabled) window.API = API;
-};
+	let isDev = process.env.NODE_ENV === 'development';
+	API.settings.dev = (enabled = null) => {
+		if (enabled === null) return isDev;
+		isDev = enabled;
+		if (enabled) window.API = API;
+	};
 
-exports.__esModule = true;
-exports.default = API;
+	module.exports = API;
+}
